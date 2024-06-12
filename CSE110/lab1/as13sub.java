67ee264a11ea3115b0d136cd365ff235663029a0
ASHIQUR RAHMAN ANKAN <129540345+ashiqurankan@users.noreply.github.com>
2024-06-12 21:04:44 +0600

    Add files via upload

---
+++ CSE110/lab1/as13sub.java
@@ -0,0 +1,29 @@
+public class as13sub{
+    public static void main(String[]args){
+        int n = 5 ; 
+
+        int t1 = n*1;
+        int t2 = n*2;
+        int t3 = n*3;
+        int t4 = n*4;
+        int t5 = n*5;
+        int t6 = n*6;
+        int t7 = n*7;
+        int t8 = n*8;
+        int t9 = n*9;
+        int t10 = n*10 ;
+
+        System.out.println(n+ " x "+ 1 + " = " + t1);
+        System.out.println(n+ " x "+ 2 + " = " + t2);
+        System.out.println(n+ " x "+ 3 + " = " + t3);
+        System.out.println(n+ " x "+ 4 + " = " + t4);
+        System.out.println(n+ " x "+ 5 + " = " + t5);
+        System.out.println(n+ " x "+ 6 + " = " + t6);
+        System.out.println(n+ " x "+ 7 + " = " + t7);
+        System.out.println(n+ " x "+ 8 + " = " + t8);
+        System.out.println(n+ " x "+ 9 + " = " + t9);
+        System.out.println(n+ " x "+ 10 + " = " + t10);
+
+
+    }
+}
